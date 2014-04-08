e291b1468a5e115d81a84b9e0f1548209aa7a606
pydev <pydev@mac19.gilbertschool.org>
2014-04-08 10:03:58 -0400

    Learn Eclipse on home mac

---
+++ MusicSync/gen/com/alecssoft/musicsync/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.alecssoft.musicsync;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ NO/MyFirstApp/gen/org/alecsoft/myfirstapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.alecsoft.myfirstapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
